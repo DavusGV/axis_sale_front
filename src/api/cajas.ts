34eb85af6289da740add9f6258158dcc57cf04b8
Reynald <reynaldochavarrias66@gmail.com>
2026-01-29 20:57:39 -0600

    history

--- src/api/cajas.ts
+++ src/api/cajas.ts
@@ -6,3 +6,15 @@ export async function fetchCajas() {
   })
   return res.data // { data: [...], total: ... }
 }
+
+export async function fetchHistoryBoxes(boxId: number) {
+  const res = await axiosInstance.get(`/cajas/${boxId}/history`, {
+  })
+  return res.data
+}
+
+ export async function fetchHistorySales(historyId: number) {
+  const res = await axiosInstance.get(`/cajas/${historyId}/sales`, {
+  })
+  return res.data
+}
